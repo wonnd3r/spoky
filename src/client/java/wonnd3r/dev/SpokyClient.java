63c791a1a9739b49d3bf2729cd95aa8b5b8d3dcb
wonnd3r <114332644+wonnd3r@users.noreply.github.com>
2024-07-20 18:13:29 +0200

    Add files via upload

---
+++ src/client/java/wonnd3r/dev/SpokyClient.java
@@ -0,0 +1,27 @@
+// INFO: REGISTER AND IMPORT CLIENT-SIDE STUFF \\
+
+package wonnd3r.dev;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+import wonnd3r.dev.sound.CaveSoundPlayer;
+import wonnd3r.dev.sound.LowHealthSoundPlayer;
+import wonnd3r.dev.sound.SoundsRegister;
+
+public class SpokyClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+
+		// CaveSoundPlayer.java class
+		CaveSoundPlayer playerPosition = new CaveSoundPlayer();
+		playerPosition.registerPositionLogger();
+
+		// LowHealthSoundPlayer.java class
+		LowHealthSoundPlayer lowHealthSoundPlayer = new LowHealthSoundPlayer();
+
+		// Register all the sounds from the SoundRegister.java class
+		SoundsRegister.registerSounds();
+
+	}
+}
---
+++ src/client/java/wonnd3r/dev/sound/LowHealthSoundPlayer.java
@@ -0,0 +1,62 @@
+// INFO: CLASS TO PLAY A HEARTBEAT WHEN THE PLAYER IS LOW \\
+
+package wonnd3r.dev.sound;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.sound.PositionedSoundInstance;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+@SuppressWarnings("ALL")
+public class LowHealthSoundPlayer {
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    int debounce = 0;
+
+    // Run updatePlayerHealth each second
+    private void startHealthUpdateScheduler() {
+        scheduler.scheduleAtFixedRate(this::updatePlayerHealth, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private void updatePlayerHealth() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.world != null && client.player != null) {
+            float playerHealth = getPlayerHealth(client);
+            BlockPos pos = client.player.getBlockPos();
+            if (playerHealth <= 4 && debounce == 0) {
+                debounce = 1;
+                playSound_heart2(client, pos);
+            } if (playerHealth >= 4) {
+                debounce = 0;
+                stopSound_heart(client);
+            }
+        }
+    }
+
+    private float getPlayerHealth(MinecraftClient client) {
+        if (client.player != null) {
+            PlayerEntity player = client.player;
+            return player.getHealth();
+        }
+        return -1;
+    }
+
+    // Register to SpokyClient
+    public LowHealthSoundPlayer() {
+        startHealthUpdateScheduler();
+    }
+
+    //Play the sound and stop it
+    private PositionedSoundInstance heartSoundInstance;
+
+    private void playSound_heart2(MinecraftClient client, BlockPos pos) {
+        heartSoundInstance = PositionedSoundInstance.master(SoundsRegister.HEART_EVENT, 1.0F, 1.0F);
+        client.getSoundManager().play(heartSoundInstance);
+    }
+    private void stopSound_heart(MinecraftClient client) {
+        client.getSoundManager().stop(heartSoundInstance);
+    }
+}
