6045ef6a964f841f85378a91c970978d813ce3ae
wonnd3r <114332644+wonnd3r@users.noreply.github.com>
2024-07-21 21:39:50 +0200

    1.1.0

---
+++ src/main/java/wonnd3r/dev/SpokyDataGenerator.java
@@ -0,0 +1,11 @@
+package wonnd3r.dev;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpokyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
